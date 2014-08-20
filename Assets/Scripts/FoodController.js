6f764cb6d9b4eb0460c252883627915484094fb2
Hiroyuki Iijima <f.work111@gmail.com>
2014-08-20 21:35:46 +0900

    フードに対してマウス操作追加 ウェブプレイヤー向けビルドディレクトリ追加

--- Assets/Scripts/FoodController.js
+++ Assets/Scripts/FoodController.js
@@ -13,8 +13,11 @@ function Update () {
 	if(Input.touchCount > 0){
 		var touchPoint : Vector2 = Camera.main.ScreenToWorldPoint(Input.GetTouch(0).position);
 	}
+	if(Input.GetMouseButton(0)){
+		touchPoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+	}
 	
-	if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began){
+	if((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began) || Input.GetMouseButtonDown(0) ){
 		var touchObject : Collider2D  = Physics2D.OverlapPoint(touchPoint);
 		if(touchObject){
 			var hitObject : RaycastHit2D = Physics2D.Raycast(touchPoint,-Vector2.up);
@@ -25,14 +28,14 @@ function Update () {
 		}
 	}
 	
-	if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved){
+	if((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved) || Input.GetMouseButton(0) ){
 		if(foodBagState == false){
 			return;
 		}
 		cornClone.transform.position = touchPoint;	
 	}
 
-	if(Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended){
+	if((Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended) || Input.GetMouseButtonUp(0) ){
 		//cornClone.transform.position = touchPoint;
 		foodBagState = false;
 	}
