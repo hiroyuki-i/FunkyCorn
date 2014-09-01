e012067a64d91c109fbe3b7f9dafe923ac35bd9e
Hiroyuki Iijima <f.work111@gmail.com>
2014-09-01 13:13:22 +0900

    FC-5 ニワトリの動きを再現する

--- Assets/Scripts/Niwatori.js
+++ Assets/Scripts/Niwatori.js
@@ -1,11 +1,13 @@
 ﻿#pragma strict
 
+private var movingStepInterval : float = 0.0;
 private var countActiveCornInPrevFrame : int = 0;
-private var isEating : boolean = false;
-private var startTime : float;
+//private var isFood : boolean = false;
 private var targetCorn : GameObject;
+private var anime : Animator;
 
 function Start(){
+	anime = GetComponent(Animator);
 }
 
 function Update () {
@@ -13,35 +15,6 @@ function Update () {
 		return;
 	}
 	randomPositioning();
-}
-
-function randomPositioning(){
-	var rand = parseInt(Random.Range(0.0 ,500.0));
-	switch(rand){
-		case 1 :
-			var x = 0;
-			var y = 1;
-			break;
-		case 2 :
-			x = 0;
-			y = -1;
-			break;
-		case 3 :
-			x = -1;
-			y = 0;
-			break;
-		case 4 :
-			x = 1;
-			y = 0;
-			break;
-		default :
-			x = 0;
-			y = 0;
-			break;
-	}
-	if(x != 0 || y != 0){
-		transform.position += new Vector3(x * 0.5, y * 0.5, 0);
-	}
 }
 
 function isFood(){
@@ -95,7 +68,7 @@ function isFood(){
 				}
 			}
 		}
-		Debug.Log(gameObject.GetInstanceID() + ":" + chickenCountToVacantestCorn);
+		
 		if(chickenCountToVacantestCorn < chickenCountInNearestCorn && chickenCountToVacantestCorn == 0){
 			targetCorn = Corns[vacantestCornIndex];
 		}else{
@@ -103,20 +76,72 @@ function isFood(){
 		}
 		
 		targetCorn.GetComponent(Corn).chickenEntry();
-		startTime = Time.time;	//SmoothStep param.
 	}
 	
-	if(isEating == false){
-		var t = (Time.time - startTime) / 8.0;
-		transform.position = Vector3(
-								Mathf.SmoothStep(transform.position.x,targetCorn.gameObject.transform.position.x,t),
-								Mathf.SmoothStep(transform.position.y,targetCorn.gameObject.transform.position.y,t),0);
+	movingStepInterval += Time.deltaTime;
+	if(movingStepInterval > 0.05){
+		if(anime.GetBool("isEating") == false){
+			var fromX = transform.position.x;
+			var fromY = transform.position.y;
+			var x = (fromX < targetCorn.gameObject.transform.position.x) ? 1 : -1;
+			var y = (fromY < targetCorn.gameObject.transform.position.y) ? 1 : -1;
+			var distanceX =  Vector3.Distance(new Vector3(fromX + 0.4 * x , fromY , 0),targetCorn.gameObject.transform.position);
+			var distanceY =  Vector3.Distance(new Vector3(fromX ,fromY + 0.4 * y , 0),targetCorn.gameObject.transform.position);	
+			if(distanceX < distanceY){
+				y = 0;
+			}else{
+				x = 0;
+			}
+			move(x,y,true);
+		}
+		movingStepInterval = 0;
 	}
 	return true;
 }
 
+function randomPositioning(){
+	var rand = parseInt(Random.Range(0.0 ,500.0));
+	switch(rand){
+		case 1 :
+			var x = 0;
+			var y = 1;
+			break;
+		case 2 :
+			x = 0;
+			y = -1;
+			break;
+		case 3 :
+			x = -1;
+			y = 0;
+			break;
+		case 4 :
+			x = 1;
+			y = 0;
+			break;
+		default :
+			x = 0;
+			y = 0;
+			break;
+	}
+	if(x != 0 || y != 0){
+		this.move(x , y , false);
+	}
+}
+
+function move(x : int, y : int, run){
+	anime.SetInteger("x",x);
+	anime.SetInteger("y",y);
+	if(run){
+		anime.speed = 1;
+	}else{
+		anime.speed = 0.2;
+	}
+	transform.position += new Vector3(x * 0.4, y * 0.4, 0);
+}
+
 function OnTriggerEnter2D(c : Collider2D){
 	if(c.gameObject.transform.name == "Corn(Clone)"){
+		anime.SetBool("isEating",true);
 		audio.Play();
 	}
 }
@@ -130,5 +155,10 @@ function OnTriggerStay2D(c : Collider2D){
 function OnTriggerExit2D(c : Collider2D){
 	if(c.gameObject.transform.name == "Corn(Clone)"){
 		targetCorn = null;
+		anime.SetBool("isEating",false);
 	}
+}
+
+function OnNiwatoriEatEnd(){
+	anime.SetBool("isEating",false);
 }
